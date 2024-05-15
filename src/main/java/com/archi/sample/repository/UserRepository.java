e19dc88b52fdd682ef42e50818534836a5f87d17
bmhtech <62375327+bmhtech@users.noreply.github.com>
2024-05-15 16:19:14 +0530

    fix delete causing server error

--- src/main/java/com/archi/sample/repository/UserRepository.java
+++ src/main/java/com/archi/sample/repository/UserRepository.java
@@ -2,13 +2,19 @@ package com.archi.sample.repository;
 
 import com.archi.sample.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM user WHERE email=?", nativeQuery = true)
     public User findByEmail(String email);
-    @Query(value = "DELETE FROM user WHERE email=?", nativeQuery = true)
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM user WHERE email=:email", nativeQuery = true)
     public void deleteByEmail(String email);
 }
